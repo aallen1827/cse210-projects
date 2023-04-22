f8065f7543f94e927088b44c09ff41123537007b
aallen1827 <all22050@byui.edu>
2023-04-22 08:23:24 -0600

    Completed Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter = "";
+
+        Console.Write("What is your grade? ");
+        int numberGrade = int.Parse(Console.ReadLine());
+        if (numberGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (numberGrade >= 80 && numberGrade < 90)
+        {
+            letter = "B";
+        }
+        else if (numberGrade >= 70 && numberGrade < 80)
+        {
+            letter = "C";
+        }
+        else if (numberGrade >= 60 && numberGrade < 70)
+        {
+            letter = "D";
+        }
+        else if (numberGrade < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You got a {letter}.");
+
+        if (numberGrade >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass the class. Keep trying.");
+        }
     }
 }
