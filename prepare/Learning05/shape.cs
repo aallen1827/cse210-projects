d5dc675a527b21143d3b41d725dfaeeeef8fa1b7
aallen1827 <all22050@byui.edu>
2023-06-14 12:42:24 -0600

    Completed Learning05

---
+++ prepare/Learning05/shape.cs
@@ -0,0 +1,20 @@
+public abstract class Shape
+{
+    private string _color;
+
+    public string GetColor()
+    {
+        return _color;
+    }
+    public void SetColor(string newColor)
+    {
+        _color = newColor;
+    }
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public abstract double GetArea();
+}
